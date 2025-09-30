ee747cf3b387989f6c0044f927a5e2692147c63b
Ethan Bastian <nw1728@ethans-mbp.wifi.saxion.nl>
2025-09-30 11:08:43 +0200

    finished week 5 assignment 1 DSA
    need to do assignment 2 and 5 DSA
    need ot do notes for DSA week 5

--- AlgoDS/5/week5-code/heap_remove/main.cpp
+++ AlgoDS/5/week5-code/heap_remove/main.cpp
@@ -1,5 +1,38 @@
+#include <algorithm>
+#include <functional>
 #include <iostream>
+#include <vector>
 #include "utils.h"
+
+void sift_down(std::vector<int>& heaps, int i) {
+    size_t n = heaps.size();
+    while (true) {
+        int smallest= i;
+        size_t left = 2*i + 1;
+        size_t right = 2*i + 2;
+        
+        if (left < n && heaps[left] < heaps[smallest]) smallest = left;
+        if (right < n && heaps[right] < heaps[smallest]) smallest = right;
+
+        if (smallest == i) break;
+
+        std::swap(heaps[i], heaps[smallest]);
+        i = smallest;   
+    }
+}
+
+
+void sift_up(std::vector<int>& heap, size_t i) {
+    while (i > 0) {
+        size_t parent = (i - 1) / 2;
+        if (heap[i] < heap[parent]) {
+            std::swap(heap[i], heap[parent]);
+            i = parent;
+        } else {
+            break;
+        }
+    }
+}
 
 int main() {
     /* TODO:
@@ -15,5 +48,33 @@ int main() {
         Example output:
             [3, 4, 10, 23, 90]
     */
+   
+    std::vector<int> heaps;
+    int toremove;
+ 
+    std::cin >> heaps >> toremove;
+
+   
+    auto where = std::find(heaps.begin(), heaps.end(), toremove);
+    if (where != heaps.end()) {
+        size_t id_where = where - heaps.begin();
+        std::swap(heaps[id_where], heaps.back());
+        heaps.pop_back();
+
+        
+    if (id_where < heaps.size()) {
+        if (id_where > 0 && heaps[id_where] < heaps[(id_where-1)/2])
+            sift_up(heaps, id_where);
+        else
+            sift_down(heaps, id_where);
+}
+
+    }
+
+    
+    
+
+    std::cout << heaps << std::endl;
     return 0;
+
 }
