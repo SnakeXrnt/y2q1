d1418156ecbdfa2102a8ce481ac3c38c825b7446
Coxaexs <coxaexs@gmail.com>
2025-10-04 12:25:55 +0200

    finished coordinats

--- AlgoDS/5/week5-code/coordinates/main.cpp
+++ AlgoDS/5/week5-code/coordinates/main.cpp
@@ -1,6 +1,105 @@
 #include <iostream>
+#include <functional>
+#include <unordered_map>
+#include <climits>
+#include "utils.h"
+
+struct Point {
+    int x;
+    int y;
+    
+
+    bool operator==(const Point& other) const {
+        return x == other.x && y == other.y;
+    }
+};
+
+namespace std { // must be in std namespace
+    template <>                 // specialization of std::hash for Point
+    struct hash<Point> {        // defines std::hash<Point>
+        size_t operator()(const Point& p) const {       // the () operator to make it callable (note the const!)
+            return hash<int>()(p.x) ^ (hash<int>()(p.y) << 1); // Combine hashes of x and y
+        }
+    };
+}
 
 int main() {
+    std::unordered_map<Point, int> visit_count;
+    std::unordered_map<Point, int> visit_order;  
+    Point coordinates = {0, 0};
+    int order_counter = 0;
+    
+    visit_count[coordinates] = 1;
+    visit_order[coordinates] = order_counter++;
+    
+    char c;
+    while (std::cin >> c)
+    {
+        if (c == 'U')
+        {
+            coordinates.y++;
+        }
+        else if (c == 'D')
+        {
+            coordinates.y--;
+        }
+        else if (c == 'L')
+        {
+            coordinates.x--;
+        }
+        else if (c == 'R')
+        {
+            coordinates.x++;
+        }
+        
+        if (visit_count.find(coordinates) == visit_count.end()) {
+            visit_order[coordinates] = order_counter++;
+        }
+        visit_count[coordinates]++;
+    }
+    
+    Point top1, top2, top3;
+    bool found1 = false, found2 = false, found3 = false;
+    
+    for (const auto& pair : visit_count) {
+        Point point = pair.first;
+        int count = pair.second;
+        int order = visit_order[point];
+        
+        if (!found1 || count > visit_count[top1] || 
+            (count == visit_count[top1] && order < visit_order[top1])) {
+            if (found1) {
+                if (found2) {
+                    top3 = top2;
+                    found3 = true;
+                }
+                top2 = top1;
+                found2 = true;
+            }
+            top1 = point;
+            found1 = true;
+        }
+        else if (!found2 || count > visit_count[top2] || 
+                 (count == visit_count[top2] && order < visit_order[top2])) {
+            if (found2) {
+                top3 = top2;
+                found3 = true;
+            }
+            top2 = point;
+            found2 = true;
+        }
+        else if (!found3 || count > visit_count[top3] || 
+                 (count == visit_count[top3] && order < visit_order[top3])) {
+            top3 = point;
+            found3 = true;
+        }
+    }
+    
+        std::cout << "(" << top1.x << ", " << top1.y << ")";
+        std::cout << ", (" << top2.x << ", " << top2.y << ")";
+        std::cout << ", (" << top3.x << ", " << top3.y << ")";
+    std::cout << std::endl;
+    
     /* TODO:
         Write a program that inputs a string consisting of the characters "U", "D", "L", and "R",
         representing movements Up, Down, Left, and Right on a 2D grid, starting from the origin (0, 0).
