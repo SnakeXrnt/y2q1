a4de4b681020e87fc55097ce69e3327fbf08bcce
SnakeXrnt <ethanbastian11@gmail.com>
2025-10-24 15:54:12 +0200

    start of relearning algods

--- AlgoDS/6/week6-code/counting_paths/main.cpp
+++ AlgoDS/6/week6-code/counting_paths/main.cpp
@@ -1,15 +1,26 @@
+#include "utils.h"
 #include <iostream>
-#include "utils.h"   // for reading vectors
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// for reading vectors
 
-int main() {
-    /* TODO:
-        Write a program that reads a list of edges representing an *undirected* graph from
-        its standard input (given as a comma-separated list between square brackets),
-        followed by two integers representing the start and end nodes.
+int count_paths(int current, int end, )
 
-        The program must then compute and print the number of *different* "simple" paths
-        from the start node to the end node. A simple path is a path that does not visit
-        any node more than once - in other words, cycles are not allowed.
-    */ 
-    return 0;
+    int main() {
+  /* TODO:
+      Write a program that reads a list of edges representing an *undirected*
+     graph from its standard input (given as a comma-separated list between
+     square brackets), followed by two integers representing the start and end
+     nodes.
+
+      The program must then compute`` and print the number of *different*
+     "simple" paths from the start node to the end node. A simple path is a path
+     that does not visit any node more than once - in other words, cycles are
+     not allowed.
+  */
+
+  return 0;
 }
